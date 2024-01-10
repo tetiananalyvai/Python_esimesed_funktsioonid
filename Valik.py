58b00ebdbfa36b6a633f41a178a630e745117c33
opilane <opilane@HP-CZC2349HT2>
2024-01-10 15:53:12 +0200

    Valik.py

---
+++ Valik.py
@@ -0,0 +1,23 @@
+from random import *
+a=randint(-100,200)
+if a%2==0:
+    print("Juhuslik arv on",a)
+    print(a,"-paaris arv")
+
+if a>0:
+    print(a,"suurem kui 0")
+else:
+    print(a,"väiksem kui 0 või võrdne 0-ga")
+#a=int(input("Protsent:"))
+#<0,>100 ei soobi, 0-59-"2",60-75-"3",76-90-"4",91-100-"5"
+if a<0 or a>100:
+    print("Viga tulemustega!")
+elif a>=0 and a<60:
+    print("Hinne 2")
+elif a>=60 and a<76:
+    print("Hinna 3")
+elif a>=76 and a<91:
+    print("Hinna 4")
+else:
+    print("Hinna 5")
+
