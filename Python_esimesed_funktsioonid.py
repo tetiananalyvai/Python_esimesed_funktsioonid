bd0d8d06956760454539e4951b79c8f300b7286b
opilane <opilane@HP-CZC2349HT2>
2024-01-10 10:03:36 +0200

    Add project files.

---
+++ Python_esimesed_funktsioonid.py
@@ -0,0 +1,52 @@
+#print("Tere maailm")
+#nimi=input("Mis on sinu nimi?")
+#vanus=int(input("Kui vana sa oled?"))
+
+#print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana.")
+#print("Tere",nimi,"! Sa oled",vanus,"aastat vana.")
+#print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+
+#print(type(nimi))
+#print(type(vanus))
+
+#arv1=float(input("Arv 1: "))
+#t=input("Tehe: ")
+#arv2=float(input("Arv 2: "))
+#vastus=eval(str(arv1)+t+str(arv2))
+#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+#print("Muutuja",vanus,"on",type(vanus))
+#print("Muutuja",eesnimi,"on",type(eesnimi))
+#print("Muutuja",pikkus,"on",type(pikkus))
+#print("Muutuja",kas_käib_koolis,"on",type(kas_käib_koolis))
+
+#from random import * #kõik funktsioonid mis on moodulis
+#kogus=randint(1,100) #66
+#print("Kokku on",kogus,"kommid")
+#mitu=int(input("Mitu kommi tahad võtta")) #33
+#kogus=kogus-mitu #kogus=kogus-mitu->kogus-=mitu
+#print("Laua peal on",kogus,"kommid")
+
+#from math import *
+#l=float(input("Ümbermõõt:"))
+#d=round(l/pi,2)    #3.14...
+#print("Läbimõõt=", d)
+
+#7
+a1=randint(1,50)    
+a2=randint(1,50)
+a3=randint(1,50)
+a4=randint(1,50)
+a5=randint(1,50)
+keskmine=(a1+a2+a3+a4+a5)/5
+print("Arvud:" a1,a2,a3,a4,a5,"nende keskmine on")
+
+
+   print("@..@")
+ print("(----)")
+ print("( \__/).center(10))
+print("^^ "" ^^".center(10))  
---
+++ Vigade.otsing.ru
@@ -0,0 +1,28 @@
+from math import *
+print("Ruudu karakteristikud")
+a=floatinput(('Sisesta ruudu külje pikkus => ')
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print("Ruudu ümbermõõt'', P)
+di=a*math.sqr(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud"))
+b=int("Sisesta ristküliku 1. külje pikkus => ")
+c=int("Sisesta ristküliku 2. külje pikkus => ")
+S=b*c
+print(Ristküliku pindala', S)
+P=2(b+c)
+print("Ristküliku ümbermõõt", P)
+di=math.sqrt(b*2+c*2)
+print("Ristküliku diagonaal", round(di)
+print()
+print("Ringi karakteristikud")
+r=int(''Sisesta ringi raadiusi pikkus => ''))
+d=2r
+print("Ringi läbimõõt" d)
+S=pi()*r*2
+print("Ringi pindala", round(S))
+C=2pi()*r
+print("Ringjoone pikkus", round(C)
